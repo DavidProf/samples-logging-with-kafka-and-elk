adb340aa94933dc5a2a493dbe8410d5aa1ca6650
David Lima <david.lima50@etec.sp.gov.br>
2021-08-25 10:13:22 -0300

    feat: init logger util

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { logger } from './logger'
+
+logger.emerg('test')
+logger.alert('test')
+logger.crit('test')
+logger.error('test')
+logger.warning('test')
+logger.warn('test')
+logger.notice('test')
+logger.info('Cheese is too ripe!')
+logger.debug('test')
